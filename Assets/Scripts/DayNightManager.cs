bb26920e1c09a14f0dcfe0bb9ee602246820938c
sebastianpacurar <pacurar.sebastian90@gmail.com>
2023-06-28 02:24:43 +0300

    add day-night cycle manager:   - days based on seconds: 1 min = 1 day; therefore 60 = 1 day; 180 = 3 days (ambiguous for now)   - linear interpolation for day phase vs night phase, manipulating global light2D intensity   - lamp swing stop through smooth damp on the ChainAndLamp object, based on minIntensity threshold   - toggle between LampOn and LampOff objects after 0.05f intensity offset

---
+++ Assets/Scripts/DayNightManager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class DayNightManager : MonoBehaviour {
+    [SerializeField] private float maxIntensity;
+    [SerializeField] private float minIntensity;
+    [SerializeField] private float cycleDuration; // how long should a day last, in seconds
+    [SerializeField] private float currentTime; // do not change
+
+    private Light2D _globalLight;
+
+    private void Start() {
+        _globalLight = GameObject.FindGameObjectWithTag("GlobalLight").GetComponent<Light2D>();
+        _globalLight.intensity = maxIntensity;
+        StartCoroutine(StartCycle());
+    }
+
+    private IEnumerator StartCycle() {
+        while (true) {
+            // daytime - ranges from 0 to cycleDuration 
+            while (currentTime < cycleDuration / 2f) {
+                currentTime += Time.deltaTime;
+                /*
+                 * NOTE:
+                 * with currentTime ranging from 0 to "cycleDuration / 2f" during the daytime phase,
+                 *   and "cycleDuration / 2f" represents the duration of the daytime phase.
+                 * currentTime / (cycleDuration / 2f) = range from 0 to 1 for daytime
+                 * 
+                */
+                var t = currentTime / (cycleDuration / 2f);
+                _globalLight.intensity = Mathf.Lerp(minIntensity, maxIntensity, t);
+
+                yield return null;
+            }
+
+            // night - ranges from "cycleDuration / 2f" to "cycleDuration".
+            while (currentTime < cycleDuration) {
+                currentTime += Time.deltaTime;
+                /* NOTE:
+                 * subtract cycleDuration / 2f from currentTime to shift the range to start from 0.
+                 * currentTime - cycleDuration / 2f) / (cycleDuration / 2f) = range from 0 to 1 for nighttime
+                */
+                var t = (currentTime - cycleDuration / 2f) / (cycleDuration / 2f);
+                _globalLight.intensity = Mathf.Lerp(maxIntensity, minIntensity, t);
+
+                yield return null;
+            }
+
+            // Reset time for the next cycle
+            currentTime = 0f;
+        }
+    }
+}
--- Assets/Scripts/Prop/Uninteractables/LampPostSwing.cs
+++ Assets/Scripts/Prop/Uninteractables/LampPostSwing.cs
@@ -1,17 +1,52 @@
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 namespace Prop.Uninteractables {
-    public class LampPostSwing : MonoBehaviour {
-        [SerializeField] private GameObject swingObject;
+    public class LampPostLogic : MonoBehaviour {
+        [Header("Light On / Off")]
+        [SerializeField] private GameObject lightOnObj;
+        [SerializeField] private GameObject lightOffObj;
+        [SerializeField] private float minIntensity; // set when min intensity for day-night transition
+
+        [Header("Lamp Swing")]
+        [SerializeField] private GameObject swingObj;
         [SerializeField] private float sinFreq;
         [SerializeField] private float sinAmplitude;
+        [SerializeField] private float stopSmoothTime;
+
+        private Light2D _globalLight;
+
+        // use as ref for SmoothDampAngle
+        private float _rotVelocity = 0f;
+
+        private void Start() {
+            _globalLight = GameObject.FindGameObjectWithTag("GlobalLight").GetComponent<Light2D>();
+        }
 
         private void Update() {
-            // calculate the rotation based on Sin movement
-            var rotZ = Mathf.Sin(Time.time * sinFreq) * sinAmplitude + swingObject.transform.localRotation.z;
+            HandleSwing();
+            HandleLampTurnOnOff();
+        }
 
-            // apply rotation using Quaternion.Euler
-            swingObject.transform.localRotation = Quaternion.Euler(0f, 0f, rotZ);
+        private void HandleSwing() {
+            // daytime
+            if (_globalLight.intensity < minIntensity) {
+                // calculate the rotation based on Sin movement
+                var rotZ = Mathf.Sin(Time.time * sinFreq) * sinAmplitude + swingObj.transform.localRotation.z;
+                swingObj.transform.localRotation = Quaternion.Euler(0f, 0f, rotZ);
+            }
+            // nighttime
+            else {
+                // stop swinging using smoothness
+                var rotZ = Mathf.SmoothDampAngle(swingObj.transform.localRotation.eulerAngles.z, Quaternion.Euler(0f, 0f, 0f).z, ref _rotVelocity, stopSmoothTime);
+                swingObj.transform.localRotation = Quaternion.Euler(0f, 0f, rotZ);
+            }
+        }
+
+        private void HandleLampTurnOnOff() {
+            // cause lamp to turn off after the swinging is over, and z rotation of swingObj equals to 0
+            lightOnObj.SetActive(_globalLight.intensity < minIntensity + 0.05f);
+            lightOffObj.SetActive(_globalLight.intensity > minIntensity + 0.05f);
         }
     }
 }
